71df5522b430894ab9a14cb5adb911a96af03f45
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-02-22 11:50:18 +0530

    first commit

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,29 @@
+empty_file.o: \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/misc/empty_file.c \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/boards/xtensa/esp32_devkitc_wrover/esp32_devkitc_wrover.dts \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/dts/xtensa/espressif/esp32/esp32_wrover_e_n4r8.dtsi \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/dts/xtensa/espressif/esp32/esp32_common.dtsi \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/dts/common/mem.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/dts/xtensa/xtensa.dtsi \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/dts/common/skeleton.dtsi \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/clock/esp32_clock.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/interrupt-controller/esp-xtensa-intmux.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/pinctrl/esp32-pinctrl.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/boards/xtensa/esp32_devkitc_wrover/esp32_devkitc_wrover-pinctrl.dtsi \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/pinctrl/esp-pinctrl-common.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_macro.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_internal.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_loops.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_listify.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/pinctrl/esp32-gpio-sigmap.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/tejasmk-tkp/Documents/RTOS_Learn/zephyr/include/zephyr/dt-bindings/input/esp32-touch-sensor-input.h
